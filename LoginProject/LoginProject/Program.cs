417f661bfb8bf917a969b18e70aed528f68e1f02
NeetuDM <neetu.dhoundiyal.vuw@gmail.com>
2022-08-31 13:06:44 +1200

    Code for Edit added

--- LoginProject/LoginProject/Program.cs
+++ LoginProject/LoginProject/Program.cs
@@ -44,32 +44,36 @@ IWebElement TimeandMaterialoption = driver.FindElement(By.XPath("/html/body/div[
 TimeandMaterialoption.Click();
 
 //Identify Create new button and click
-IWebElement CreateNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
-CreateNewButton.Click();
+IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+createNewButton.Click();
 
 //Identify TypeCode and select Time
-IWebElement TypeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\'TimeMaterialEditForm\']/div/div[1]/div/span[1]/span/span[1]"));
-TypeCodeDropdown.Click();
+IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\'TimeMaterialEditForm\']/div/div[1]/div/span[1]/span/span[1]"));
+typeCodeDropdown.Click();
+Thread.Sleep(3000);
+IWebElement timeOption = driver.FindElement(By.XPath("//*[@id=\'TypeCode_listbox\']/li[2]"));
+timeOption.Click();
+Thread.Sleep(3000);
 
 //Identify Code Textbox and enter value
-IWebElement CodeButton = driver.FindElement(By.Id("Code"));
-CodeButton.SendKeys("Neet2022");
+IWebElement codeButton = driver.FindElement(By.Id("Code"));
+codeButton.SendKeys("Neet2022");
 
 //Identigy Description Textbox and enter value
-IWebElement DescriptionTextBox = driver.FindElement(By.Id("Description"));
-DescriptionTextBox.SendKeys("Data");
+IWebElement descriptionTextBox = driver.FindElement(By.Id("Description"));
+descriptionTextBox.SendKeys("Data");
 
 //Identify Price per unit and enter value
-IWebElement PriceperunitTag = driver.FindElement(By.XPath("//*[@id=\'TimeMaterialEditForm\']/div/div[4]/div/span[1]/span/input[1]"));
-PriceperunitTag.Click();
+IWebElement pricePerunitTag = driver.FindElement(By.XPath("//*[@id=\'TimeMaterialEditForm\']/div/div[4]/div/span[1]/span/input[1]"));
+pricePerunitTag.Click();
 
-IWebElement PriceperunitTextbox = driver.FindElement(By.Id("Price"));
-PriceperunitTextbox.SendKeys("78");
+IWebElement pricePerunitTextbox = driver.FindElement(By.Id("Price"));
+pricePerunitTextbox.SendKeys("78");
 
 //Identify Save button and click
 
-IWebElement SaveButton = driver.FindElement(By.Id("SaveButton"));
-SaveButton.Click();
+IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
 Thread.Sleep(3000);
 
 //Got the the last page to Check if the new record is present in the table
@@ -98,34 +102,36 @@ IWebElement TimeandmaterialTab = driver.FindElement(By.XPath("/html/body/div[3]/
 TimeandmaterialTab.Click();
 
 //Identify Create New Button and CLick
-IWebElement CreatenewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
-CreatenewButton.Click();
+IWebElement createnewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+createnewButton.Click();
 
 //Identify CodeTextBox and Enter value
-IWebElement CodeTextBox = driver.FindElement(By.Id("Code"));
-CodeTextBox.SendKeys("Sept");
+IWebElement codeTextBox = driver.FindElement(By.Id("Code"));
+codeTextBox.SendKeys("Sept");
 
 //Identify Description TextBox and Enter value
-IWebElement DescriptionBox = driver.FindElement(By.Id("Description"));
-DescriptionBox.SendKeys("Auto123");
+IWebElement descriptionBox = driver.FindElement(By.Id("Description"));
+descriptionBox.SendKeys("Auto123");
 
 //Identify PriceperUnitTextBox and Enter value
-IWebElement PricePerUnitTag = driver.FindElement(By.XPath("//*[@id=\'TimeMaterialEditForm\']/div/div[4]/div/span[1]/span/input[1]"));
-PricePerUnitTag.Click();
-IWebElement PricePUTextBox = driver.FindElement(By.Id("Price"));
-PricePUTextBox.SendKeys("200");
+IWebElement pricePerUnitTag = driver.FindElement(By.XPath("//*[@id=\'TimeMaterialEditForm\']/div/div[4]/div/span[1]/span/input[1]"));
+
+pricePerUnitTag.Click();
+IWebElement pricePUTextBox = driver.FindElement(By.Id("Price"));
+pricePUTextBox.SendKeys("200");
 Thread.Sleep(2000);
 
 // Identify Save Button and click
-IWebElement SaveButtonA = driver.FindElement(By.Id("SaveButton"));
-SaveButtonA.Click();
+IWebElement saveButtonA = driver.FindElement(By.Id("SaveButton"));
+saveButtonA.Click();
 Thread.Sleep(2000);
 
 //Got to the last page and check if new Material data added to the table
 
-//Click on Go to the Last page
-IWebElement GototheLastPage = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
-GototheLastPage.Click();
+//Click on Go to the Last page pagination button
+IWebElement gototheLastPage = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+gototheLastPage.Click();
+
 IWebElement NewRecord = driver.FindElement(By.XPath("//*[@id=\'tmsGrid\']/div[3]/table/tbody/tr[last()]/td[1]"));
 
 //Check if new Material record added to the last page
@@ -140,3 +146,58 @@ else
 {
     Console.WriteLine("Material record has not been added, test failed");
 }
+
+
+//Edit T&M (First record)
+//Click on the Gotothe First page and Identify Edit button and Click
+IWebElement GototheFirstPage = driver.FindElement(By.XPath("//*[@id=\'tmsGrid\']/div[4]/a[1]/span"));
+GototheFirstPage.Click();
+IWebElement EditButton = driver.FindElement(By.XPath("//*[@id=\'tmsGrid\']/div[3]/table/tbody/tr[1]/td[5]/a[1]"));
+EditButton.Click();
+
+//Identify CodeTextBox and Edit Record
+IWebElement codeEditTextBox= driver.FindElement(By.Id("Code"));
+codeEditTextBox.Clear();
+codeEditTextBox.SendKeys("Manual");
+Thread.Sleep(2000);
+
+//Identify DescriptionTextBox and Edit value
+IWebElement descriptionEditTextBox = driver.FindElement(By.Id("Description"));
+descriptionEditTextBox.Clear();
+descriptionEditTextBox.SendKeys("NewZealand");
+Thread.Sleep(2000);
+
+//Identify PriceperunitEditTag
+IWebElement pricePerUnitEditTag = driver.FindElement(By.XPath("//*[@id=\'TimeMaterialEditForm\']/div/div[4]/div/span[1]/span/input[1]"));
+pricePerUnitEditTag.Click();
+Thread.Sleep(3000);
+
+//Identify PricePUTextBOX and Edit Value
+IWebElement priceEditTextBox = driver.FindElement(By.Id("Price"));
+priceEditTextBox.Clear();
+//priceEditTextBox.SendKeys("1000");
+Thread.Sleep(3000);
+
+//Identify SaveButton and Click
+IWebElement saveEditButton = driver.FindElement(By.Id("SaveButton"));
+saveEditButton.Click();
+Thread.Sleep(4000);
+
+//Check if the First record on the TimeMaterial page edited sucessfully
+IWebElement codeRecord1 = driver.FindElement(By.XPath("//*[@id=\'tmsGrid\']/div[3]/table/tbody/tr[1]/td[1]"));
+IWebElement descriptionRecord = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[1]/td[3]"));
+
+
+if (codeRecord1.Text == "Manual" && descriptionRecord.Text == "NewZealand")
+
+
+{
+    Console.WriteLine("Record Edited Sucessfully, Test Passed");
+}
+else
+
+{
+    Console.WriteLine("Record has not been edited, Test failed");
+}
+
+
