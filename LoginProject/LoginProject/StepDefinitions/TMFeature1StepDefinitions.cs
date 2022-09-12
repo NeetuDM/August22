ee0810684365fa0ada3ad7f9a61c422d876c507d
NeetuDM <neetu.dhoundiyal.vuw@gmail.com>
2022-09-12 14:03:19 +1200

    Specflow-feature and stepdefinitions folder implemented

---
+++ LoginProject/LoginProject/StepDefinitions/TMFeature1StepDefinitions.cs
@@ -0,0 +1,37 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace LoginProject.StepDefinitions
+{
+    [Binding]
+    public class TMFeature1StepDefinitions
+    {
+        [Given(@"I logged into turn up portal successfully")]
+        public void GivenILoggedIntoTurnUpPortalSuccessfully()
+        {
+           
+
+        }
+
+        [When(@"I navigate to the time and material page")]
+        public void WhenINavigateToTheTimeAndMaterialPage()
+        {
+            
+
+        }
+
+        [When(@"I create a new time and material record")]
+        public void WhenICreateANewTimeAndMaterialRecord()
+        {
+           
+
+        }
+
+        [Then(@"The record should be create successfully")]
+        public void ThenTheRecordShouldBeCreateSuccessfully()
+        {
+            
+
+        }
+    }
+}
